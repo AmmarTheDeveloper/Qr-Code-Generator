d722e3f40034b4a413dae73d4f4a56609ce1cfea
AmmarTheDeveloper <141614147+AmmarTheDeveloper@users.noreply.github.com>
2023-08-24 21:18:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ const userName = document.querySelector( '#name' )
 const userNumber = document.querySelector( '#number' )
 const RollNo = document.querySelector("#roll-no")
 const userAge = document.querySelector( '#age' )
+const ilaqa = document.querySelector("#ilaqa")
 
 button.onclick = () => {
 
-    if ( userName.value.length < 3 || userNumber.value.length != 10 || userAge.value.length == 0 ) {
+    if ( userName.value.length < 3 || userNumber.value.length != 10 || userAge.value.length == 0 || RollNo.value.length == 0 || ilaqa.value.length == 0) {
         alert( 'Please enter all fields properly' )
         return
     }
 
-    let userValues = `{Participant:"${ userName.value }",Contact : "${ userNumber.value }" , Registeration_No:"${ userAge.value }"}`
+    let userValues = `{RollNo : "${RollNo.value}" , Participant:"${ userName.value }", Contact : "${ userNumber.value }" , ilaqa : "${ilaqa.value}" , Registeration_No:"${ userAge.value }"}`
 
     let qrCode = `${ api }${ userValues }`;
     img.src = qrCode;
